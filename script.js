b749c92cbd80120bda835051d9c718491983942e
Cullen Steber <steber@uab.edu>
2022-02-07 09:33:11 -0600

    add character length slider

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-function run() {
-    let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let pwLength = 12;
-    let password = "";
-  
-    const array = new Uint32Array(chars.length);
-    window.crypto.getRandomValues(array);
-  
-    for (let i = 0; i < pwLength; i++) {
-      password += chars[array[i] % chars.length];
-    }
-  
-    const inputF = document.getElementById("resultfield");
-    inputF.value = password;
+function run(value) {
+  let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  document.getElementById('pwdRangeSlider').innerHTML = value;
+  let pwLength = value;
+  let password = "";
+
+  const array = new Uint32Array(chars.length);
+  window.crypto.getRandomValues(array);
+
+  for (let i = 0; i < pwLength; i++) {
+    password += chars[array[i] % chars.length];
   }
-  
-  function copyReField() {
-    var ttc = document.getElementById("resultfield");
-    ttc.select();
-    ttc.setSelectionRange(0,99999);
-    navigator.clipboard.writeText(ttc.value);
-    ttc.value = "Password Copied to Clipboard!";
-  }
+
+  const resultField = document.getElementById("resultfield");
+  resultField.value = password;
+}
+
+function copyReField() {
+  var ttc = document.getElementById("resultfield");
+  ttc.select();
+  ttc.setSelectionRange(0,99999);
+  navigator.clipboard.writeText(ttc.value);
+  ttc.value = "Password Copied to Clipboard!";
+}
